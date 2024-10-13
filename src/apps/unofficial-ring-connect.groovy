40fb54e36ce1bd9ac03ec2455078406d67a17e14
Bill Bierman <wbierman@users.noreply.github.com>
2024-10-13 09:06:17 -1000

    Fixed dingable device detection

--- src/apps/unofficial-ring-connect.groovy
+++ src/apps/unofficial-ring-connect.groovy
@@ -614,7 +614,7 @@ def uninstalled() {
 }
 
 void setupDingables() {
-  state.dingables = getChildDevices()?.findAll { RINGABLES.contains(it.getDataValue("kind")) }?.collect { getRingDeviceId(it.deviceNetworkId) }
+  state.dingables = getChildDevices()?.findAll { DINGABLES.contains(it.getDataValue("kind")) }?.collect { getRingDeviceId(it.deviceNetworkId) }
 }
 
 void configureDingPolling() {
